43f6e2718c25651aa0f13c5cf086971a3c43270a
shekhAzharuddin <talktoskazhar786@gmail.com>
2025-02-10 11:42:39 +0530

    learning react file structure

---
+++ 01BasicsReact/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Sample from "./sample.jsx"
+
+function App() {
+
+
+  return (
+    <Sample/>
+  )
+}
+
+export default App
---
+++ 01BasicsReact/src/sample.jsx
@@ -0,0 +1,7 @@
+function Sample (){
+    return(
+        <h2>sample test of react</h2>
+    )
+}
+
+export default Sample
